e5d6bdc8625bfde35cdaf5bf5022983105e27170
Lionel <lraseemela@gmail.com>
2023-07-30 22:45:50 +0200

    Added GUI's for Logging in Vendor, missing functionality to access info from the database to veify if the user exists.

--- app/src/main/java/com/example/campusfoodexpress/LoginActivity.java
+++ app/src/main/java/com/example/campusfoodexpress/LoginActivity.java
@@ -6,28 +6,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
-public class RegisterVendorActivity extends AppCompatActivity {
-    EditText edtBusinessName,edtContactNumber,pickStartTime,pickEndTime,
-            edtClosestBuilding,edtBusinessDescription,edtUsername,edtPassword,edtConfirmPassword;
-    TextView txtErrorOutputMessage;
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_vendor);
+        setContentView(R.layout.activity_login);
 
         //set actionbar title Dynamically
         ActionBar actionBar = getSupportActionBar();
         if(actionBar!=null){
-            actionBar.setTitle("Registering Vendor...");
+            actionBar.setTitle("Logging in");
         }
     }
 
     public void onCancelClicked(View view) {
-        Intent intent = new Intent(RegisterVendorActivity.this,SignupActivity.class);
+        Intent intent = new Intent(LoginActivity.this,WelcomeActivity.class);
         startActivity(intent);
     }
 }
