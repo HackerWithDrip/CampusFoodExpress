0356678220e3c5e6ce28b0c11a4fdbc6d7295efb
Lionel <lraseemela@gmail.com>
2023-08-03 20:13:53 +0200

    Added dashboard screen for the vendor, need to change a few colors

--- app/src/main/java/com/example/campusfoodexpress/MainActivity.java
+++ app/src/main/java/com/example/campusfoodexpress/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.campusfoodexpress;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,10 +21,15 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this,"Welcome!",Toast.LENGTH_LONG).show();
         }else{
             actionBar.setTitle("Dashboard");
-            Toast.makeText(this,"Logged in successfully!",Toast.LENGTH_LONG).show();
+            Toast.makeText(this,"Logged in successfully!",Toast.LENGTH_SHORT).show();
         }
 
 
-        // I will start here
+    }
+
+    public void onLogOutClicked(View view) {
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+        Toast.makeText(this,"You are now offline!",Toast.LENGTH_LONG).show();
     }
 }
